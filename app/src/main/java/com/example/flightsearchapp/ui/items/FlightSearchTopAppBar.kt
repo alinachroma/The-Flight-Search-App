5f7fa4c0557eb74e8fe80059e0a8a183708763db
achroma <alinachroma@gmail.com>
2024-08-19 17:31:33 +0200

    add TopAppBar color

--- app/src/main/java/com/example/flightsearchapp/ui/items/FlightSearchTopAppBar.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/items/FlightSearchTopAppBar.kt
@@ -3,8 +3,11 @@ package com.example.flightsearchapp.ui.items
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.example.flightsearchapp.R
 import com.example.flightsearchapp.utils.ThemePreviews
@@ -18,7 +21,10 @@ fun FlightSearchTopAppBar(modifier: Modifier = Modifier) {
                 text = stringResource(id = R.string.app_name),
                 modifier = modifier
             )
-        }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color.LightGray
+        )
     )
 }
 
