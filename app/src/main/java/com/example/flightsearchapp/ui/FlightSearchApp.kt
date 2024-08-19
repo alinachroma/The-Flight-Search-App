6d914daf0561a49b75a14eb37dd633d65afa1350
achroma <alinachroma@gmail.com>
2024-08-19 15:04:55 +0200

    cleanup

--- app/src/main/java/com/example/flightsearchapp/ui/FlightSearchApp.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/FlightSearchApp.kt
@@ -1,14 +1,13 @@
 package com.example.flightsearchapp.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import com.example.flightsearchapp.R
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.flightsearchapp.R
 import com.example.flightsearchapp.ui.screens.HomeScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
