daed2a33f179d2cc2ed8c38b57d00a652914b393
achroma <alinachroma@gmail.com>
2024-08-19 15:04:51 +0200

    add top app bar

---
+++ app/src/main/java/com/example/flightsearchapp/ui/items/FlightSearchTopAppBar.kt
@@ -0,0 +1,28 @@
+package com.example.flightsearchapp.ui.items
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.flightsearchapp.R
+import com.example.flightsearchapp.utils.ThemePreviews
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FlightSearchTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name)
+            )
+        }
+    )
+}
+
+@ThemePreviews
+@Composable
+fun FlightSearchTopAppBarPreview() {
+    FlightSearchTopAppBar()
+}
