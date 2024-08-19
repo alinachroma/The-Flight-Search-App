f5d01b9dcaefd71b06dbed19363a9668d8007db0
achroma <alinachroma@gmail.com>
2024-08-19 17:27:57 +0200

    add a VM that is responsible for a search logic

---
+++ app/src/main/java/com/example/flightsearchapp/ui/SearchViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.flightsearchapp.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flightsearchapp.data.Airport
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+class SearchViewModel : ViewModel() {
+
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
+
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
+}
+
