f231d62c5ba0bdca96198ed552432e74fa946bbf
achroma <alinachroma@gmail.com>
2024-08-20 17:52:51 +0200

    wire the dao function

--- app/src/main/java/com/example/flightsearchapp/data/AirportRepository.kt
+++ app/src/main/java/com/example/flightsearchapp/data/AirportRepository.kt
@@ -5,9 +5,13 @@ import kotlinx.coroutines.flow.Flow
 
 interface AirportRepository {
     fun getAirportsStream(): Flow<List<Airport>>
+    fun getAirportByQuery(query: String): Flow<List<Airport>>
 }
 
 class FlightSearchAirportRepository(private val airportDao: AirportDao): AirportRepository {
     override fun getAirportsStream(): Flow<List<Airport>> =
         airportDao.getAllAirports()
+
+    override fun getAirportByQuery(query: String): Flow<List<Airport>> =
+        airportDao.getAirportQuery(query)
 }
