c3a044233c68a2245a21f3d0c7ed01ec308a1184
achroma <alinachroma@gmail.com>
2024-08-20 13:58:54 +0200

    add a database class

---
+++ app/src/main/java/com/example/flightsearchapp/data/FlightSearchDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.flightsearchapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.flightsearchapp.model.Airport
+import com.example.flightsearchapp.model.Favorite
+
+@Database(entities = [Airport::class, Favorite::class], version = 1, exportSchema = false)
+abstract class FlightSearchDatabase: RoomDatabase() {
+
+    abstract fun airportDao(): AirportDao
+    abstract fun favoriteDao(): FavoriteDao
+
+    companion object {
+        @Volatile
+        private var Instance: FlightSearchDatabase? = null
+
+        fun getDatabase(context: Context): FlightSearchDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, FlightSearchDatabase::class.java, "flight_search")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
