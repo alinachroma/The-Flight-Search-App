40cb30c926fe2444c598a27c163e4a654334cf76
achroma <alinachroma@gmail.com>
2024-08-20 18:12:58 +0200

    remove unneeded query

--- app/src/main/java/com/example/flightsearchapp/data/AirportRepository.kt
+++ app/src/main/java/com/example/flightsearchapp/data/AirportRepository.kt
@@ -4,14 +4,10 @@ import com.example.flightsearchapp.model.Airport
 import kotlinx.coroutines.flow.Flow
 
 interface AirportRepository {
-    fun getAirportsStream(): Flow<List<Airport>>
     fun getAirportByQuery(query: String): Flow<List<Airport>>
 }
 
 class FlightSearchAirportRepository(private val airportDao: AirportDao): AirportRepository {
-    override fun getAirportsStream(): Flow<List<Airport>> =
-        airportDao.getAllAirports()
-
     override fun getAirportByQuery(query: String): Flow<List<Airport>> =
         airportDao.getAirportQuery(query)
 }
