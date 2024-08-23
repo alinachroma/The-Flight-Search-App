d27dee8d61bf0be751047f846d42a6e2dcaebfc8
achroma <alinachroma@gmail.com>
2024-08-23 14:15:24 +0200

    add a repository for favorite routes

--- app/src/main/java/com/example/flightsearchapp/FlightSearchApplication.kt
+++ app/src/main/java/com/example/flightsearchapp/FlightSearchApplication.kt
@@ -2,11 +2,16 @@ package com.example.flightsearchapp
 
 import android.app.Application
 import com.example.flightsearchapp.data.FlightSearchDatabase
-import com.example.flightsearchapp.data.FlightSearchAirportRepository
+import com.example.flightsearchapp.data.OfflineAirportRepository
+import com.example.flightsearchapp.data.OfflineFavoriteRouteRepository
 
 class FlightSearchApplication: Application() {
     private val database: FlightSearchDatabase by lazy { FlightSearchDatabase.getDatabase(this) }
+
     val airportRepository by lazy {
-        FlightSearchAirportRepository(database.airportDao())
+        OfflineAirportRepository(database.airportDao())
+    }
+    val favoriteRouteRepository by lazy {
+        OfflineFavoriteRouteRepository(database.favoriteRoutesDao())
     }
 }
--- app/src/main/java/com/example/flightsearchapp/data/FavoriteRouteRepository.kt
+++ app/src/main/java/com/example/flightsearchapp/data/FavoriteRouteRepository.kt
@@ -1,4 +1,26 @@
 package com.example.flightsearchapp.data
 
+import com.example.flightsearchapp.model.FavoriteRoute
+import kotlinx.coroutines.flow.Flow
+
 interface FavoriteRouteRepository {
+    fun getFavoriteRoutes(): Flow<List<FavoriteRoute>>
+    suspend fun insert(favoriteRoute: FavoriteRoute)
+    suspend fun delete(favoriteRoute: FavoriteRoute)
+}
+
+class OfflineFavoriteRouteRepository(private val favoriteRouteDao: FavoriteRouteDao) : FavoriteRouteRepository {
+
+    override fun getFavoriteRoutes(): Flow<List<FavoriteRoute>> =
+        favoriteRouteDao.getFavoriteRoutes()
+
+
+    override suspend fun insert(favoriteRoute: FavoriteRoute) =
+        favoriteRouteDao.insertFavoriteRoute(favoriteRoute)
+
+
+    override suspend fun delete(favoriteRoute: FavoriteRoute) =
+        favoriteRouteDao.deleteFavoriteRoute(favoriteRoute)
+
+
 }
