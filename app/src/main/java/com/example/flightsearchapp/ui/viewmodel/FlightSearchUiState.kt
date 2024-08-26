2752e2b612db7681b1f777dc6d0a382a5fe76f51
achroma <alinachroma@gmail.com>
2024-08-26 18:37:45 +0200

    add favorite routes list to the uiState

--- app/src/main/java/com/example/flightsearchapp/ui/viewmodel/FlightSearchUiState.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/viewmodel/FlightSearchUiState.kt
@@ -1,11 +1,13 @@
 package com.example.flightsearchapp.ui.viewmodel
 
 import com.example.flightsearchapp.model.Airport
+import com.example.flightsearchapp.model.FavoriteRoute
 
 data class FlightSearchUiState(
     val searchText: String = "",
     val isSearching: Boolean = false,
     val airports: List<Airport> = emptyList(),
     val isAirportSelected: Boolean = false,
-    val selectedAirport: Airport? = null
+    val selectedAirport: Airport? = null,
+    var favoriteRoutes: List<FavoriteRoute> = emptyList(),
 )
