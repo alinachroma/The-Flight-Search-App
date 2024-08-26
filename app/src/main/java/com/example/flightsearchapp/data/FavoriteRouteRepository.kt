74edfcd6956a80011ee0b4f473dadeffa2a67939
achroma <alinachroma@gmail.com>
2024-08-26 18:32:51 +0200

    wire new Delete query

--- app/src/main/java/com/example/flightsearchapp/data/FavoriteRouteRepository.kt
+++ app/src/main/java/com/example/flightsearchapp/data/FavoriteRouteRepository.kt
@@ -6,21 +6,20 @@ import kotlinx.coroutines.flow.Flow
 interface FavoriteRouteRepository {
     fun getFavoriteRoutes(): Flow<List<FavoriteRoute>>
     suspend fun insertFavoriteRoute(favoriteRoute: FavoriteRoute)
-    suspend fun deleteFavoriteRoute(favoriteRoute: FavoriteRoute)
+    suspend fun deleteFavoriteRoute(departureIata: String, destinationIata: String)
 }
 
-class OfflineFavoriteRouteRepository(private val favoriteRouteDao: FavoriteRouteDao) : FavoriteRouteRepository {
+class OfflineFavoriteRouteRepository(private val favoriteRouteDao: FavoriteRouteDao) :
+    FavoriteRouteRepository {
 
     override fun getFavoriteRoutes(): Flow<List<FavoriteRoute>> =
         favoriteRouteDao.getFavoriteRoutes()
 
-
     override suspend fun insertFavoriteRoute(favoriteRoute: FavoriteRoute) =
         favoriteRouteDao.insertFavoriteRoute(favoriteRoute)
 
-
-    override suspend fun deleteFavoriteRoute(favoriteRoute: FavoriteRoute) =
-        favoriteRouteDao.deleteFavoriteRoute(favoriteRoute)
+    override suspend fun deleteFavoriteRoute(departureIata: String, destinationIata: String) =
+        favoriteRouteDao.deleteFavoriteRoute(departureIata, destinationIata)
 
 
 }
