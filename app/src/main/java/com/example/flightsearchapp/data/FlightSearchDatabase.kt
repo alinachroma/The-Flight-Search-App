fc763895db1d507e49ab13c875b6a5387873a6e2
achroma <alinachroma@gmail.com>
2024-08-26 17:38:06 +0200

    get rid of the logic responsible for wiping the db

--- app/src/main/java/com/example/flightsearchapp/data/FlightSearchDatabase.kt
+++ app/src/main/java/com/example/flightsearchapp/data/FlightSearchDatabase.kt
@@ -21,7 +21,6 @@ abstract class FlightSearchDatabase: RoomDatabase() {
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, FlightSearchDatabase::class.java, "flight_search")
                     .createFromAsset("database/flight_search.db")
-                    .fallbackToDestructiveMigration()
                     .build()
                     .also { Instance = it }
             }
