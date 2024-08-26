120cddc3f85a3c950061344a9063a9850bd8bc7a
achroma <alinachroma@gmail.com>
2024-08-26 18:36:59 +0200

    favorite icon refactoring

--- app/src/main/java/com/example/flightsearchapp/ui/items/FavoriteIcon.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/items/FavoriteIcon.kt
@@ -3,35 +3,27 @@ package com.example.flightsearchapp.ui.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.flightsearchapp.R
 import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
 import com.example.flightsearchapp.utils.ThemePreviews
 
 @Composable
 fun FavoriteIcon(
-    onFavoriteRouteClicked: (Boolean) -> Unit,
+    onFavoriteRouteClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
 
-    IconToggleButton(
-        checked = isFavoriteRoute,
-        onCheckedChange = {
-            isFavoriteRoute = !isFavoriteRoute
-            onFavoriteRouteClicked(isFavoriteRoute)
-        },
-        modifier = modifier
-    ) {
+    IconButton(
+        modifier = modifier,
+        onClick = { onFavoriteRouteClicked() }) {
         Icon(
-            tint = if (isFavoriteRoute) Color.Red else Color.LightGray,
-            imageVector = Icons.Filled.Star,
-            contentDescription = null
+            imageVector = Icons.Default.Star,
+            //tint = if (isFavoriteRoute) Color.Red else Color.LightGray,
+            contentDescription = stringResource(id = R.string.favorite)
         )
     }
 }
@@ -41,7 +33,6 @@ fun FavoriteIcon(
 fun FavoriteIconPreview() {
     FlightSearchAppTheme {
         FavoriteIcon(
-            hasFavorite = true,
             onFavoriteRouteClicked = {}
         )
     }
