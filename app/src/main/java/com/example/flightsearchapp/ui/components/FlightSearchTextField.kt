b2fb2decfb8200c1661112c441611ae234d840ec
achroma <alinachroma@gmail.com>
2024-09-02 18:03:36 +0200

    add customizable search text field

---
+++ app/src/main/java/com/example/flightsearchapp/ui/components/FlightSearchTextField.kt
@@ -0,0 +1,84 @@
+package com.example.flightsearchapp.ui.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.flightsearchapp.R
+
+@Composable
+fun FlightSearchTextField(
+    modifier: Modifier = Modifier,
+    searchText: String,
+    onSearchTextChange: (String) -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(id = R.dimen.padding_medium))
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null
+            ) {
+                keyboardController?.hide()
+                focusManager.clearFocus(true)
+            }
+            .shadow(
+                ambientColor = Color.Blue,
+                spotColor = Color.Cyan,
+                elevation = if (isFocused) 15.dp else 0.dp,
+                clip = true,
+                shape = CircleShape
+            ),
+        shape = CircleShape
+    ) {
+        TextField(
+            value = searchText,
+            onValueChange = onSearchTextChange,
+            interactionSource = interactionSource,
+            placeholder = { Text(text = stringResource(R.string.user_input)) },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    width = 2.dp,
+                    brush = Brush.horizontalGradient(
+                        listOf(Color.Cyan, Color.Blue)
+                    ),
+                    shape = CircleShape
+                )
+                .focusRequester(focusRequester)
+        )
+    }
+}
--- app/src/main/java/com/example/flightsearchapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/screens/HomeScreen.kt
@@ -1,31 +1,21 @@
 package com.example.flightsearchapp.ui.screens
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.dimensionResource
 import com.example.flightsearchapp.R
 import com.example.flightsearchapp.model.Airport
 import com.example.flightsearchapp.model.FavoriteRoute
-import com.example.flightsearchapp.ui.components.AirportInfoItem
 import com.example.flightsearchapp.ui.components.AirportsListItem
 import com.example.flightsearchapp.ui.components.FavoriteRoutesItem
-import com.example.flightsearchapp.ui.components.RouteItem
+import com.example.flightsearchapp.ui.components.FlightSearchTextField
 import com.example.flightsearchapp.ui.components.RoutesForSelectedAirportItem
 import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
 import com.example.flightsearchapp.utils.ThemePreviews
@@ -50,13 +40,14 @@ fun HomeScreen(
     Column(
         modifier = modifier.fillMaxSize(),
     ) {
-        TextField(
-            value = searchText,
-            onValueChange = onSearchTextChange,
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text(text = stringResource(R.string.user_input)) }
+        FlightSearchTextField(
+            searchText = searchText,
+            onSearchTextChange = onSearchTextChange
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(
+            modifier = Modifier
+                .height(dimensionResource(id = R.dimen.padding_medium))
+        )
         if (isSearching) {
             Box(
                 modifier = Modifier.fillMaxSize()
