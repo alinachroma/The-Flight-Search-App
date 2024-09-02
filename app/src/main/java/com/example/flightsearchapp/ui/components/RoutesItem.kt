549a07f0f5205979aa5c9f31a8b429879ce9fdda
achroma <alinachroma@gmail.com>
2024-09-02 19:51:50 +0200

    add round corners to a card

--- app/src/main/java/com/example/flightsearchapp/ui/components/RoutesItem.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/components/RoutesItem.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,7 +29,11 @@ fun RouteItem(
     onFavoriteRouteClicked: (FavoriteRoute) -> Unit,
     isFavoriteButtonFilled: (FavoriteRoute) -> Boolean,
 ) {
-    Card {
+    Card(
+        shape = RoundedCornerShape(
+            topEndPercent = 15
+        )
+    ) {
         Row(
             modifier = modifier
                 .padding(8.dp)
