3496e1807f33f96cce2d5930d11b5b7b375e1a0d
achroma <alinachroma@gmail.com>
2024-09-04 16:12:40 +0200

    refactor favorite button

--- app/src/main/java/com/example/flightsearchapp/ui/components/buttons/FavoriteButton.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/components/buttons/FavoriteButton.kt
@@ -1,5 +1,6 @@
 package com.example.flightsearchapp.ui.components.buttons
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
@@ -7,7 +8,9 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.flightsearchapp.R
 import com.example.flightsearchapp.model.FavoriteRoute
 import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
@@ -29,6 +32,7 @@ fun FavoriteButton(
         }
     ) {
         Icon(
+            modifier = Modifier.size(dimensionResource(id = R.dimen.icon_standard)),
             imageVector = Icons.Default.Star,
             tint = if (isFavoriteButtonFilled(route)) Color.Red else Color.LightGray,
             contentDescription = stringResource(id = R.string.favorite)
