6622e265d85cf718de2002700dff910349e9180b
achroma <alinachroma@gmail.com>
2024-09-05 17:11:58 +0200

    define ripple configuration for the icon

--- app/src/main/java/com/example/flightsearchapp/ui/components/buttons/FavoriteButton.kt
+++ app/src/main/java/com/example/flightsearchapp/ui/components/buttons/FavoriteButton.kt
@@ -3,9 +3,12 @@ package com.example.flightsearchapp.ui.components.buttons
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.BlendMode
@@ -19,8 +22,11 @@ import com.example.flightsearchapp.model.FavoriteRoute
 import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
 import com.example.flightsearchapp.ui.theme.starIconGradient
 import com.example.flightsearchapp.utils.ThemePreviews
+import com.example.flightsearchapp.utils.brush
 import com.example.flightsearchapp.utils.fakeAirportsData
+import com.example.flightsearchapp.utils.starIconRippleConfiguration
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoriteButton(
     onFavoriteRouteClicked: (FavoriteRoute) -> Unit,
@@ -28,39 +34,39 @@ fun FavoriteButton(
     route: FavoriteRoute,
     modifier: Modifier = Modifier
 ) {
-    IconButton(
-        modifier = modifier,
-        onClick = {
-            onFavoriteRouteClicked(route)
-            isFavoriteButtonFilled(route)
-        }
-    ) {
-        Icon(
-            modifier = Modifier
-                .size(dimensionResource(id = R.dimen.icon_standard))
-                .let {
-                    if (isFavoriteButtonFilled(route)) {
-                        return@let it
-                            .graphicsLayer(alpha = 0.99f)
-                            .drawWithCache {
-                                val brush = Brush.linearGradient(
-                                    starIconGradient
-                                )
-                                onDrawWithContent {
-                                    drawContent()
-                                    drawRect(
-                                        brush = brush,
-                                        blendMode = BlendMode.SrcAtop
-                                    )
+
+    CompositionLocalProvider(LocalRippleConfiguration provides starIconRippleConfiguration) {
+        IconButton(
+            modifier = modifier,
+            onClick = {
+                onFavoriteRouteClicked(route)
+                isFavoriteButtonFilled(route)
+            }
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(dimensionResource(id = R.dimen.icon_standard))
+                    .let {
+                        if (isFavoriteButtonFilled(route)) {
+                            return@let it
+                                .graphicsLayer(alpha = 0.99f)
+                                .drawWithCache {
+                                    onDrawWithContent {
+                                        drawContent()
+                                        drawRect(
+                                            brush = brush,
+                                            blendMode = BlendMode.SrcAtop
+                                        )
+                                    }
                                 }
-                            }
-                    }
-                    it
-                },
-            imageVector = Icons.Default.Star,
-            tint = Color.LightGray,
-            contentDescription = stringResource(id = R.string.favorite)
-        )
+                        }
+                        it
+                    },
+                imageVector = Icons.Default.Star,
+                tint = Color.LightGray,
+                contentDescription = stringResource(id = R.string.favorite)
+            )
+        }
     }
 }
 
